ddf2f2ccbde29f1c4832773230c2d9cb42ed3615
JackelineMatos <jackeline.matos@gmail.com>
2024-01-11 17:33:44 +0100

    Exercicio1-em-andamento

---
+++ README.md
@@ -0,0 +1,10 @@
+- [x]  A soma dos dois números;
+- [x]  A subtração dos dois números;
+- [x]  A multiplicação dos dois números;
+- [x]  A divisão dos dois números;
+- [ ]  O resto da divisão dos dois números;
+
+Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
+
+- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
---
+++ exercicio1/script.js
@@ -0,0 +1,32 @@
+let firstNumberUser = prompt ("Digite o primeiro número");
+let secondNumberUser = prompt ("Digite o segundo número");
+
+function sum(number1, number2) {
+    return alert (`A soma dos dois números é ${Number(number1) + Number(number2)}`)  
+}
+
+console.log(sum(firstNumberUser, secondNumberUser));
+
+
+function subtraction(number1, number2) {
+    return alert (`A subtracao dos dois números é ${Number(number1) - Number(number2)}`)  
+}
+console.log(subtraction(firstNumberUser, secondNumberUser));
+
+
+
+function multiplication(number1, number2){
+    return alert (`A multiplicacao dos dois números é ${Number(number1) * Number(number2)}`)  
+
+}
+console.log(multiplication(firstNumberUser, secondNumberUser));
+
+
+function division(number1, number2){
+    return alert (`A multiplicacao dos dois números é ${Number(number1) * Number(number2)}`)  
+
+}
+
+console.log(division(firstNumberUser, secondNumberUser));
+
+
