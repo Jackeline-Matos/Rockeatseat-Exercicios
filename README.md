1861129b0dbc53e712f5461170287972246622a0
JackelineMatos <jackeline.matos@gmail.com>
2024-01-11 18:13:57 +0100

    finalizacao-do-exercicios

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 - [x]  A subtração dos dois números;
 - [x]  A multiplicação dos dois números;
 - [x]  A divisão dos dois números;
-- [ ]  O resto da divisão dos dois números;
+- [x]  O resto da divisão dos dois números;
 
 Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
 
-- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
-- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
+- [x]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [x]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
--- exercicio1/script.js
+++ exercicio1/script.js
@@ -8,6 +8,8 @@ function sum(number1, number2) {
 console.log(sum(firstNumberUser, secondNumberUser));
 
 
+
+
 function subtraction(number1, number2) {
     return alert (`A subtracao dos dois números é ${Number(number1) - Number(number2)}`)  
 }
@@ -23,10 +25,30 @@ console.log(multiplication(firstNumberUser, secondNumberUser));
 
 
 function division(number1, number2){
-    return alert (`A multiplicacao dos dois números é ${Number(number1) * Number(number2)}`)  
+    return alert (`A divisao dos dois números é ${Number(number1) / Number(number2)}`)  
 
 }
 
 console.log(division(firstNumberUser, secondNumberUser));
 
 
+function remeinderDivision( number1, number2){
+    return alert (`O resto da divisao do número 1 é ${(Number(number1)% 10).toFixed(1) }. O resto da divisao do número 2 é ${(Number(number2)% 10).toFixed(1) } `)  
+}
+
+console.log(remeinderDivision(firstNumberUser, secondNumberUser));
+
+let soma = Number(firstNumberUser) + Number(secondNumberUser);
+
+if(soma % 2  === 0){
+    console.log( alert (`A Soma dos dois números é par `))
+
+}else {
+    console.log ( alert (`A Soma dos dois números é ímpar `))
+}
+
+if(Number(firstNumberUser) !== Number(secondNumberUser) ){
+    console.log(alert (`Os números digitados sao diferentes `));
+}else {
+    console.log (alert (`Os números digitados sao iguais `))
+}
